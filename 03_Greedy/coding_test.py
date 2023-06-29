eb2d26292312723b76f4c0a352449ae3fe56db43
Allie_ <55966445+LEEYUN-JU@users.noreply.github.com>
2023-06-29 14:03:24 +0900

    Add files via upload
    곱하기 혹은 더하기

---
+++ 03_Greedy/coding_test.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 09:57:09 2023
+
+@author: labpc
+"""
+       
+#상하좌우
+
+import numpy as np
+import random
+import copy
+
+n = np.random.randint(1, 20)
+
+num = []
+st = ''
+
+for i in range(n):
+    temp = np.random.randint(0, 9)
+    num.append(temp)
+    st += str(temp)
+
+num.sort(reverse=True)
+
+total = 1
+
+for j in range(0, len(num)):
+    if num[j] == 0 and j == 0:
+        total = 0
+    
+    if num[j] != 0:
+        temp_total = total * num[j]
+        
+        if temp_total > 2000000000:
+            total = total + num[j]
+        else:
+            total = temp_total
+        
+        
+    else:
+        total = total + num[j]
+        
+print(total)
+    
+
+           
+           
+            
+        
+        
+           
+            
+        
+            
+            
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
