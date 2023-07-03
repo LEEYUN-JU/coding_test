9a8e07082d8c4b1be3e085045b173d5ea6d67cfb
Allie_ <55966445+LEEYUN-JU@users.noreply.github.com>
2023-07-03 11:35:01 +0900

    Add files via upload

---
+++ 04_implementation/coding_test.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 16 09:57:09 2023
+
+@author: labpc
+"""
+       
+#
+
+import string
+import numpy as np
+import random
+
+
+letter_len = np.random.randint(1, 10000)
+
+string_pool = string.ascii_uppercase + string.digits
+letters = ''
+for i in range(letter_len):
+    letters += random.choice(string_pool)
+
+let = ''
+nums = 0
+
+for i in range(letter_len):
+    try:
+        if type(int(letters[i])) == int:
+            nums += int(letters[i])
+    
+    except:
+        let += letters[i]
+
+sorted_letter = ''.join(sorted(let))
+
+print(sorted_letter + str(nums))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+            
