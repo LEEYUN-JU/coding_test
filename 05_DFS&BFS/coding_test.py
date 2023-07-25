1e7ef467ab378af4d9144ff43b16ec85725488e8
Allie_ <55966445+LEEYUN-JU@users.noreply.github.com>
2023-07-25 09:50:56 +0900

    Add files via upload

---
+++ 05_DFS&BFS/coding_test.py
@@ -0,0 +1,38 @@
+import numpy as np
+import random
+from collections import deque
+
+n = 4 #도시의 개수
+m = 4 #도로의 개수
+k = 2 #거리 정보
+x = 1 #출발 도시의 정보
+
+roads = [[1, 2], [1, 3], [2, 3], [2, 4]]
+cities = []
+
+for road in roads:
+    temp = road
+    for test in roads:
+        if temp[1] == test[0]:
+            cities.append(test[1])
+            
+for road in roads:
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
