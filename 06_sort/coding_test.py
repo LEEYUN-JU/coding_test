15eef2f929f227cb31fb8cb67182e68feccf12bb
Allie_ <55966445+LEEYUN-JU@users.noreply.github.com>
2023-09-13 14:41:44 +0900

    Add files via upload

---
+++ 06_sort/coding_test.py
@@ -0,0 +1,36 @@
+from collections import deque
+import numpy as np
+import random
+
+n = 2
+
+temp = [["홍길동", 95], ["이순신", 77]]
+
+temp = sorted(temp, key=lambda student: student[1])
+
+for student in temp:
+    print(student[0], end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+    
+   
