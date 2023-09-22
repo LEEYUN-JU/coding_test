47198ac600a13f55ec3a79be4fc09f6f5f680241
Allie_ <55966445+LEEYUN-JU@users.noreply.github.com>
2023-09-22 15:58:25 +0900

    Add files via upload

---
+++ 07_binary_search/coding_test.py
@@ -0,0 +1,44 @@
+from collections import deque
+import numpy as np
+import random
+import heapq
+
+n = 7
+x = 4
+
+lisst = [1,1,2,2,2,2,3]
+nums = 0
+
+if lisst.count(x) == True:
+    nums = lisst.count(x)
+    
+else:
+    nums = -1
+
+print(nums)
+        
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+            
